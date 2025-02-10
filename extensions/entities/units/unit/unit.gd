fa56fe135564e6523b27d99b82b534946f21b522
meinfesl <meinfesl@gmail.com>
2025-02-10 23:18:54 +0300

    Scared Sausage fix

--- extensions/entities/units/unit/unit.gd
+++ extensions/entities/units/unit/unit.gd
@@ -2,24 +2,14 @@ extends "res://entities/units/unit/unit.gd"
 
 
 func _on_BurningTimer_timeout()->void:
-	var sausage = false
-	
-	if _burning && _burning.type != BurningType.ENGINEERING:
-		sausage = true
-		if _burning.from && is_instance_valid(_burning.from):
-			for effect in _burning.from.effects:
-				if effect.key == "effect_burning":
-					sausage = false
-					break
-	
 	var hp = current_stats.health
 	
 	RunData.mod_advstats.burn = true
-	RunData.mod_advstats.sausage = sausage
+	RunData.mod_advstats.sausage = _burning.is_global_burn
 	._on_BurningTimer_timeout()
 	RunData.mod_advstats.sausage = false
 	RunData.mod_advstats.burn = false
 	
-	if sausage:
+	if _burning.is_global_burn:
 		RunData.mod_advstats.run_stats["DAMAGE_SAUSAGE"] += (hp - current_stats.health)
 
